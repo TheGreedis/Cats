cc96d395115559b1cc339783bb7c09aa19a5c3ba
TheGreedis <jasp3r233@gmail.com>
2024-10-17 00:14:04 +0500

    Добавлен комбобокс для выбора тегов. Поле Entry удалено.

--- Cataas.py
+++ Cataas.py
@@ -1,7 +1,11 @@
 from tkinter import *
+from tkinter import ttk
 from PIL import Image, ImageTk
 import requests
 from io import BytesIO
+
+
+Allowed_tags = ["sleep", "jump", "fight", "black", "white", "bengal", "siamese", "cute"]
 
 
 def load_image(url):
@@ -18,7 +22,7 @@ def load_image(url):
 
 
 def open_new_window():
-    tag = tag_entry.get()
+    tag = tag_combobox.get()
     url_tag = f"https://cataas.com/cat/{tag}" if tag else "https://cataas.com/cat"
     img = load_image(url_tag)
 
@@ -40,14 +44,6 @@ window.title("Cats!")
 window.geometry("600x520")
 
 
-tag_entry = Entry()
-tag_entry.pack()
-
-
-load_button = Button(text="Загрузить по тегу", command=open_new_window)
-load_button.pack()
-
-
 menu_bar = Menu(window)
 window.config(menu=menu_bar)
 
@@ -60,6 +56,18 @@ file_menu.add_command(label="Выход", command=exit)
 
 
 url = "https://cataas.com/cat"
+
+
+tag_label = Label(text="Выбери тэг")
+tag_label.pack()
+
+
+tag_combobox = ttk.Combobox(values=Allowed_tags)
+tag_combobox.pack()
+
+
+load_button = Button(text="Загрузить по тегу", command=open_new_window)
+load_button.pack()
 
 
 window.mainloop()
